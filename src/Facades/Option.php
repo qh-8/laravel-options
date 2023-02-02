a9b601e6beffa8997b057be99797275136e29115
Dinh Quoc Han <dqh@dinhquochan.com>
2023-02-02 11:09:53 +0700

    fix: docblock facade

--- src/Facades/Option.php
+++ src/Facades/Option.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\Facade;
  * @method static void boot()
  * @method static bool has(string $key)
  * @method static mixed|null get(string $key, mixed|null $default = null)
- * @method static void set(array|string $key, void $value = null, bool|null $autoload = null, bool|null $isLock = null)
+ * @method static void set(array|string $key, mixed $value = null, bool|null $autoload = null, bool|null $isLock = null)
  * @method static void setMany(array $values, bool|null $autoload = null, bool|null $isLock = null)
  * @method static void lock(array|string $key)
  * @method static void unlock(array|string $key)
